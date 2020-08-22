2020a7162ae493127b4bc32d1905ed41a043ae68
e7ast1c <e7atic@github.com>
2020-08-23 00:57:12 +0300

    add echo web sockets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AsideSection from './components/AsideSection.jsx'
-
+import SetupSocket from './mixins/sockets'
 
 function App() {
+  useEffect(() => {
+    SetupSocket()
+    console.log("effect")
+  });
+
   return (<div className="App" >
         <header>Header</header>
         <aside>
